7acc77e95d2e6d502e124bc779962321b12f6c78
Nikolas Görlitz <git@ngoerlitz.de>
2023-11-09 14:08:20 +0100

    fix "deactivated" not in field list

--- src/controllers/training-type/TrainingTypeController.ts
+++ src/controllers/training-type/TrainingTypeController.ts
@@ -1,38 +1,42 @@
-import { Request, Response } from "express";
+import {NextFunction, Request, Response} from "express";
 import ValidationHelper, { ValidationOptions } from "../../utility/helper/ValidationHelper";
 import { TrainingType } from "../../models/TrainingType";
 
-async function getByID(request: Request, response: Response) {
-    const trainingTypeID = request.params.id;
+async function getByID(request: Request, response: Response, next: NextFunction) {
+    try {
+        const params = request.params as {id: string};
 
-    const validation = ValidationHelper.validate([
-        {
-            name: "id",
-            validationObject: trainingTypeID,
-            toValidate: [{ val: ValidationOptions.NON_NULL }],
-        },
-    ]);
-
-    if (validation.invalid) {
-        response.status(400).send({ validation: validation.message, validation_failed: validation.invalid });
-        return;
-    }
-
-    const trainingType = await TrainingType.findOne({
-        where: {
-            id: trainingTypeID?.toString(),
-        },
-        attributes: ["id", "name", "type"],
-        include: {
-            association: TrainingType.associations.training_stations,
-            attributes: ["id", "callsign", "frequency", "deactivated"],
-            through: {
-                attributes: [],
+        const validation = ValidationHelper.validate([
+            {
+                name: "id",
+                validationObject: params.id,
+                toValidate: [{val: ValidationOptions.NON_NULL}],
             },
-        },
-    });
+        ]);
 
-    response.send(trainingType);
+        if (validation.invalid) {
+            response.status(400).send({validation: validation.message, validation_failed: validation.invalid});
+            return;
+        }
+
+        const trainingType = await TrainingType.findOne({
+            where: {
+                id: params.id,
+            },
+            attributes: ["id", "name", "type"],
+            include: {
+                association: TrainingType.associations.training_stations,
+                attributes: ["id", "callsign", "frequency"],
+                through: {
+                    attributes: [],
+                },
+            },
+        });
+
+        response.send(trainingType);
+    } catch (e) {
+        next(e);
+    }
 }
 
 export default {
