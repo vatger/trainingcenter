53cbc7b295d319b6675ba07cd62e9bcbf34be048
Nikolas Görlitz <git@ngoerlitz.de>
2023-11-09 15:29:35 +0100

    Add author to training log

--- src/controllers/training-log/TrainingLogController.ts
+++ src/controllers/training-log/TrainingLogController.ts
@@ -10,6 +10,7 @@ async function getByUUID(request: Request, response: Response, next: NextFunctio
             where: {
                 uuid: params.uuid,
             },
+            include: [TrainingLog.associations.author]
         });
 
         if (trainingLog == null) {
