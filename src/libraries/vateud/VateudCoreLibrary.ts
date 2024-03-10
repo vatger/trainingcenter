7e9b2f1fb2a4bdda03e20b98724bc6e7c4a3b2d2
Nikolas Görlitz <git@ngoerlitz.de>
2024-03-10 20:43:48 +0100

    Vateud Core Library and .env.example

--- src/libraries/vateud/VateudCoreLibrary.ts
+++ src/libraries/vateud/VateudCoreLibrary.ts
@@ -1,22 +1,37 @@
 import axios, {Method} from "axios";
 import JobLibrary, {JobTypeEnum} from "../JobLibrary";
-import {VateudCoreSoloCreateResponse, VateudCoreSoloCreateT, VateudCoreTypeEnum} from "./VateudCoreLibraryTypes";
+import {
+    VateudCoreSoloCreateResponseT,
+    VateudCoreSoloCreateT,
+    VateudCoreSoloRemoveResponseT,
+    VateudCoreSoloRemoveT,
+    VateudCoreTypeEnum
+} from "./VateudCoreLibraryTypes";
 import {Config} from "../../core/Config";
+import {UserSolo} from "../../models/UserSolo";
+import Logger, {LogLevels} from "../../utility/Logger";
 
 type SendT = {
     method: Method;
     endpoint: string;
-    data: any;
+    data?: any;
 }
 
 /**
  * Sends the actual request to VATEUD Core
  * @param props
- * @param type
  */
-async function _send<T>(props: SendT, type: VateudCoreTypeEnum): Promise<T | undefined> {
+async function _send<T>(props: SendT): Promise<T | undefined> {
+    if (Config.VATEUD_CORE_CONFIG.API_KEY == null) {
+        Logger.log(LogLevels.LOG_ERROR, `VATEUD Core API Key missing, but attempted to update: ${props.endpoint}. Aborting.`);
+        return undefined;
+    }
+
     try {
         const res = await axios({
+            headers: {
+                'x-api-key': Config.VATEUD_CORE_CONFIG.API_KEY,
+            },
             url: `${Config.URI_CONFIG.VATEUD_API_BASE}/${props.endpoint}`,
             method: props.method,
             data: props.data
@@ -28,12 +43,19 @@ async function _send<T>(props: SendT, type: VateudCoreTypeEnum): Promise<T | und
     }
 }
 
+/**
+ * Creates a solo.
+ * On success, it updates the corresponding UserSolo with the returned VATEUD Solo ID
+ * On failure, it schedules a job which repeats the same request n times until it succeeds
+ * - If it fails more than n times, then it really isn't our problem anymore tbh...
+ * @param soloInfo
+ */
 async function createSolo(soloInfo: VateudCoreSoloCreateT) {
-    const res = await _send<VateudCoreSoloCreateResponse>({
+    const res = await _send<VateudCoreSoloCreateResponseT>({
         endpoint: "/solo",
         method: "post",
-        data: soloInfo
-    }, VateudCoreTypeEnum.SOLO_CREATE);
+        data: soloInfo.post_data
+    });
     if (!res) {
         // If the request fails, we schedule a job to attempt it additional times.
         await JobLibrary.scheduleJob(JobTypeEnum.VATEUD_CORE, {
@@ -42,15 +64,52 @@ async function createSolo(soloInfo: VateudCoreSoloCreateT) {
             data: {
                 solo_create: {
                     local_solo_id: soloInfo.local_solo_id,
-                    user_id: soloInfo.user_id,
-                    position: soloInfo.position,
-                    instructor_cid: soloInfo.instructor_cid,
-                    expire_at: soloInfo.expires_at
+                    user_id: soloInfo.post_data.user_id,
+                    position: soloInfo.post_data.position,
+                    instructor_cid: soloInfo.post_data.instructor_cid,
+                    expire_at: soloInfo.post_data.expires_at
                 }
             }
         });
         return undefined;
     }
 
+    await UserSolo.update({
+        vateud_solo_id: res.data.id
+    }, {
+        where: {
+            id: soloInfo.local_solo_id
+        }
+    });
+
     return res;
+}
+
+/**
+ * Removes a solo.
+ * On failure, it schedules a job which repeats the same request n times until it succeeds
+ * @param soloInfo
+ */
+async function removeSolo(soloInfo: VateudCoreSoloRemoveT) {
+    const res = await _send<VateudCoreSoloRemoveResponseT>({
+        endpoint: `/solo/${soloInfo.vateud_solo_id}`,
+        method: "delete"
+    });
+    if (!res) {
+        await JobLibrary.scheduleJob(JobTypeEnum.VATEUD_CORE, {
+            type: VateudCoreTypeEnum.SOLO_REMOVE,
+            method: "delete",
+            data: {
+                solo_remove: {
+                    local_solo_id: soloInfo.local_solo_id,
+                    vateud_solo_id: soloInfo.vateud_solo_id
+                }
+            }
+        });
+    }
+}
+
+export default {
+    createSolo,
+    removeSolo
 }
